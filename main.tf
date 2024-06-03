f8168116d36bd91231ee2a4596ca349686b1fba7
SaratA8717 <115520069+SaratA8717@users.noreply.github.com>
2024-06-03 14:58:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "resource_group_eus2" {
-  name     = "rg-${var.name}-eus2"
-  location = var.location
+  name     = "rg-${var.rgname}"
+  location = var.rglocation
   tags = var.eus2_tags
 }
